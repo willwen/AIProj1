d726e487b403181f3775d7fac605513e25f42037
Will <will.wen1@gmail.com>
2015-01-21 16:20:29 -0500

    initial files

---
+++ src/referee/Node.java
@@ -0,0 +1,5 @@
+package referee;
+
+public class Node {
+
+}
---
+++ src/referee/SearchTree.java
@@ -0,0 +1,9 @@
+package referee;
+
+import java.util.TreeSet;
+
+public class SearchTree extends TreeSet<Node> {
+	SearchTree(int row, int column ){
+		
+	}
+}
